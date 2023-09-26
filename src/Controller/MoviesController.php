85ebe6c00e1715732c650d8b644ed2bb5aa63a3f
alpeshnakum <19bmiit068@gmail.com>
2023-09-26 11:51:01 +0530

    Dynamic Route and html value

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -4,12 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies', name: 'app_movies')]
-    public function index(): JsonResponse
+    // In route there is no need to specify methods by default it will take needed method.
+    #[Route('/movie/{movie_name}', name: 'app_movies', defaults:['movie_name' => null])]
+
+    public function index($movie_name): Response
+    {
+        return $this->render('index.html.twig', [
+            'movieTitle' => $movie_name
+        ]);
+    }
+
+    #[Route('/old', name: 'app_old')]
+    public function oldMethod(): Response
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
