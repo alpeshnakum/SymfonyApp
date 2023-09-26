96274e6b4a8bc05718c528191c6b5da46fd9057c
alpeshnakum <19bmiit068@gmail.com>
2023-09-26 12:23:18 +0530

    Twig in Symfony

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -10,21 +10,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class MoviesController extends AbstractController
 {
     // In route there is no need to specify methods by default it will take needed method.
-    #[Route('/movie/{movie_name}', name: 'app_movies', defaults:['movie_name' => null])]
+    #[Route('/movie/{movie_name}', name: 'app_movie', defaults:['movie_name' => null])]
 
     public function index($movie_name): Response
     {
         return $this->render('index.html.twig', [
+            "display" => "movie-detail",
             'movieTitle' => $movie_name
         ]);
     }
 
-    #[Route('/old', name: 'app_old')]
+    #[Route('/movies', name: 'app_movies')]
     public function oldMethod(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MoviesController.php',
-        ]);
+        $movies_list = ["Avanger: Endgame", "Inception", "Loki", "Black Widow"];
+        return $this->render('index.html.twig', array(
+            "display" => "movie-list",
+            "movies_list" => $movies_list
+        ));
     }
 }
