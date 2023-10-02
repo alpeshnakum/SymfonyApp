c9dcce5df4131c6e630839f2c94fda0f7a35f8ef
alpeshnakum <19bmiit068@gmail.com>
2023-10-02 16:52:02 +0530

    Repositories in symfony

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     // In route there is no need to specify methods by default it will take needed method.
-    #[Route('/movie/{movie_name}', name: 'app_movie', defaults:['movie_name' => null])]
+    #[Route('/movie/{movie_name}', name: 'app_movie', defaults: ['movie_name' => null])]
 
     public function index($movie_name): Response
     {
@@ -20,13 +27,21 @@ class MoviesController extends AbstractController
         ]);
     }
 
+    // below is the systamic way to display data.
     #[Route('/movies', name: 'app_movies')]
     public function oldMethod(): Response
     {
-        $movies_list = ["Avanger: Endgame", "Inception", "Loki", "Black Widow"];
-        return $this->render('index.html.twig', array(
-            "display" => "movie-list",
-            "movies_list" => $movies_list
-        ));
+        // findAll() -> SLEECT * FROM movies;
+        // find(11) -> SLEECT * FROM movies WHERE id=11;
+        // findBy(['title' => 'The Dark Knight', 'column-name' => 'value'], [id => 'DESC'])
+        // findBy([], ['title' => 'DESC']) ==== sort by
+        // count([]) ==== number of rows from the result
+        // getClassName() ==== gives name/path of class(table name to be used)
+
+        $repository = $this->em->getRepository(Movie::class); //Movie::class is name of entity
+        $movies = $repository->findBy([], ['title' => 'DESC']);
+        dd($movies);
+
+        return $this->render('index.html.twig');
     }
 }
